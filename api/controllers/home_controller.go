8b4314e6688427ef734da5e893c32db94d3fc9d6
Tijani <60608240+ismail-tijani@users.noreply.github.com>
2020-06-09 05:45:51 +0200

    Update home_controller.go

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"net/http"
 
-	"github.com/Muhammad-Tounsi/Vote-Go-Vue/api/responses"
+	"github.com/ismail-tijani/Vote-Vue-Go/api/responses"
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
